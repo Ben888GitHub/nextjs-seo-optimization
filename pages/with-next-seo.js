e0b41b73fea157ed0a73ae087f49f6827f202876
Benedict <benedictryan80@gmail.com>
2022-07-05 15:37:18 +0700

    rename SEO properties

--- pages/with-next-seo.js
+++ pages/with-next-seo.js
@@ -4,10 +4,10 @@ function WithNextSeo() {
 	return (
 		<>
 			<NextSeo
-				title="Next.js SEO"
-				titleTemplate="Next.js SEO"
-				defaultTitle="Next.js SEO"
-				description="A full stack web developer, who loves to design and develop beautiful websites. I have been coding for over a year now. One of my hobbies is writing, I love to document my journey by writing blog posts and also teach others through them."
+				title="Check out this seo superpower from Next js"
+				titleTemplate="Check out this seo superpower from Next js"
+				defaultTitle="Check out this seo superpower from Next js"
+				description="Check out this seo superpower from Next js"
 				canonical="https://www.avneesh.tech/" // this is your website domain
 				openGraph={{
 					url: 'https://www.avneesh.tech/',
